68922b81bd3bb68fda324a49aefea27f9cc7e83e
prasetya <aryoprasetya60@gmail.com>
2026-06-26 14:05:21 +0700

    [ADD] Menambahkan index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba Git Remote</title>
+</head>
+<body>
+    <h1>Helo World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis quo ratione animi inventore similique quasi facilis sapiente sequi! Consequatur commodi harum veritatis aliquam soluta velit accusantium nihil perferendis sapiente nam.</p>
+</body>
+</html>
