0dfba49ead8baea0d6fab0beb2abf0d1c82d6eb2
prasetya <aryoprasetya60@gmail.com>
2026-06-28 16:58:21 +0700

    [ADD] Menambahkan kalkulator sederhana

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function tampiltanggal(){
     document.getElementById('demo').innerHTML = Date();
+    document.getElementById('demo').style.display='block';
 }
 
 function gender_pria(){
@@ -8,4 +9,42 @@ function gender_pria(){
 
 function gender_wanita(){
     document.getElementById('imggender').src='wanita.jpg';
+}
+
+function perbesar_style_img(){
+    document.getElementById('imggender').style.maxWidth='200px';
+}
+
+function perkecil_style_img(){
+    document.getElementById('imggender').style.maxWidth='100px';
+}
+
+function hide(){
+    document.getElementById('demo').style.display='none';
+}
+
+function tambah(){
+ let angka1 = Number(document.getElementById('a').value);
+ let angka2 = Number(document.getElementById('b').value);
+ let jumlah = angka1 + angka2;
+ document.getElementById('hasil').innerHTML=jumlah;
+}
+
+function kurang(){
+ let angka1 = Number(document.getElementById('a').value);
+ let angka2 = Number(document.getElementById('b').value);
+  let kurang = angka1 - angka2;
+ document.getElementById('hasil').innerHTML=(kurang);
+}
+
+function kali(){
+ let angka1 = Number(document.getElementById('a').value);
+ let angka2 = Number(document.getElementById('b').value);
+ document.getElementById('hasil').innerHTML=(angka1*angka2);
+}
+
+function bagi(){
+ let angka1 = Number(document.getElementById('a').value);
+ let angka2 = Number(document.getElementById('b').value);
+ document.getElementById('hasil').innerHTML=(angka1/angka2);
 }
