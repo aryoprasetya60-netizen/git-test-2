5ef9b556c49db765a582f17ccd2eddc23580bfac
prasetya <aryoprasetya60@gmail.com>
2026-06-28 12:52:52 +0700

    [ADD] Menambahkan script.js untuk menampilkan tanggal dan gender

---
+++ script.js
@@ -0,0 +1,11 @@
+function tampiltanggal(){
+    document.getElementById('demo').innerHTML = Date();
+}
+
+function gender_pria(){
+    document.getElementById('imggender').src='pria.jpg';
+}
+
+function gender_wanita(){
+    document.getElementById('imggender').src='wanita.jpg';
+}
